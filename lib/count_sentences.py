8f2179d6bf74ed2d624d0d0a7fade878bd4628f6
Bruce <bruce.hillsworth@gmail.com>
2023-05-03 16:07:41 -0600

    Passed tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
 
+import ipdb
+
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if type(value) is not str:
+            print("The value must be a string.")
+        else:
+            self._value = value
+
+    def is_sentence(self):
+        if self.value[-1] == ".":
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value[-1] == "?":
+            return True
+        else:
+            return False
+
+    def is_exclamation(self):
+        if self.value[-1] == "!":
+            return True
+        else:
+            return False
+
+    def count_sentences(self):
+        sentences = [
+            string.strip()
+            for string in self.value.replace("?", ".").replace("!", ".").split(".")
+            if string.strip() != ""
+        ]
+        return len(sentences)
